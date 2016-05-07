af417131f72bd80632f26c54cc61778c1bfff3b9
andy pan <panjf2000@126.com>
2016-05-07 17:43:54 +0800

    init files

---
+++ custom_handler/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+@version: 1.0
+@author: allanpan
+@license: Apache Licence 
+@contact: panjf2000@gmail.com
+@site: http://www.python.org
+@file: __init__.py.py
+@time: 2016/5/7 14:36
+@tag: 1,2,3
+@todo: ...
+
+"""
